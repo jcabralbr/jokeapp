9630c599d11a1cd4c86eb2d20978154432ca6c6b
Julio Cesar Cabral <jcabralbr@gmail.com>
2018-11-01 10:48:13 -0300

    java class configuration and xml configuration examples

--- src/main/java/guru/springframwork/joke/jokeapp/JokeappApplication.java
+++ src/main/java/guru/springframwork/joke/jokeapp/JokeappApplication.java
@@ -3,8 +3,10 @@ package guru.springframwork.joke.jokeapp;
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//@ImportResource("classpath:chuck-config.xml") using if xml configuration
 public class JokeappApplication {
 
     public static void main(String[] args) {
